18e3c31369f2de63f11d7ca6d923bb94b505eabd
Yuriy Syrovetskiy <cblp@cblp.su>
2017-06-28 17:30:02 +0300

    Remove extra module

--- src/Network/Gitter/Monad.hs
+++ src/Network/Gitter/Monad.hs
@@ -3,11 +3,10 @@ module Network.Gitter.Monad where
 import           Network.Gitter.Types
 
 import           Control.Monad.Reader
-import           Control.Monad.Trans.X
 import           Data.Aeson
 
 class Monad m => MonadGitter m where
     runGitterAction :: ResourcePath -> Value -> m Value
 
 instance MonadGitter m => MonadGitter (ReaderT r m) where
-    runGitterAction = lift2 runGitterAction
+    runGitterAction path = lift . runGitterAction path
